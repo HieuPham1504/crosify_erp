894839dc747ea4d878c7be058918e788d64147cf
Phạm Trọng Hiếu <81959458+HieuPham1504@users.noreply.github.com>
2024-01-24 11:32:27 +0700

    Update product_category.py

--- crosify_main/crosify_order/models/product_category.py
+++ crosify_main/crosify_order/models/product_category.py
@@ -12,15 +12,28 @@ class ProductCategory(models.Model):
             name = record.name
             product_category_code = record.product_category_code
 
-            duplicate_name = self.sudo().search(
-                [('id', '!=', record.id), ('name', '=', name)])
-            if duplicate_name:
-                raise ValidationError(_("This Product Category Name already exists."))
-
-            duplicate_code = self.sudo().search([('id', '!=', record.id), ('product_category_code', '=', product_category_code)])
-            if duplicate_code:
-                raise ValidationError(_("This Product Category Code already exists."))
+            if name:
+                duplicate_name = self.sudo().search(
+                    [('id', '!=', record.id), ('name_lower', '=', name.lower())])
+                if duplicate_name:
+                    raise ValidationError(_("This Product Category Name already exists."))
+            if product_category_code:
+                duplicate_code = self.sudo().search([('id', '!=', record.id), ('product_category_code_lower', '=', product_category_code.lower())])
+                if duplicate_code:
+                    raise ValidationError(_("This Product Category Code already exists."))
 
 
     name = fields.Char(trim=True)
+    name_lower = fields.Char(compute='compute_name_lower', trim=True, store=True)
     product_category_code = fields.Char(string='Product Category Code', required=True, trim=True)
+    product_category_code_lower = fields.Char(compute='compute_product_code_lower', trim=True, store=True)
+
+    @api.depends('name')
+    def compute_name_lower(self):
+        for rec in self:
+            rec.name_lower = rec.name.lower() if rec.name else False
+
+    @api.depends('name')
+    def compute_product_code_lower(self):
+        for rec in self:
+            rec.product_category_code_lower = rec.product_category_code.lower() if rec.product_category_code else False
