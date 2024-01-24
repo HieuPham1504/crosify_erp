9bb85093d991378abbc29a013e053c5831a2e550
Phạm Trọng Hiếu <81959458+HieuPham1504@users.noreply.github.com>
2024-01-24 13:57:02 +0700

    logic template code

--- crosify_main/crosify_order/models/product_template.py
+++ crosify_main/crosify_order/models/product_template.py
@@ -6,12 +6,21 @@ class ProductTemplate(models.Model):
     _inherit = 'product.template'
     _description = 'Product Type'
 
-    product_type = fields.Char(string="Product Type", related='categ_id.product_category_code', store=True, index=True)
+    product_type = fields.Char(string="Product Type", compute="compute_product_type", store=True, index=True)
     design_number = fields.Char(string='Design Number', require=True, trim=True)
 
     # def _create_variant_ids(self):
     #     return
 
+    @api.depends('design_number', 'categ_id')
+    def compute_product_type(self):
+        for rec in self:
+            categ_id = rec.categ_id
+            design_number = rec.design_number
+            if design_number and categ_id and categ_id.product_category_code:
+                rec.product_type = f'{categ_id.product_category_code}{design_number}'
+            else:
+                rec.product_type = False
     def _prepare_variant_values(self, combination):
         variant_dict = super()._prepare_variant_values(combination)
         sku_suffix = self.env['ir.sequence'].sudo().next_by_code('product.product.sku') or '_Undefined'
