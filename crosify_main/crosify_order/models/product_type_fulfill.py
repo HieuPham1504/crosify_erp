0d62ce42ec4aab2b0afb856379eb17de3e9c84b8
maitanchinh <64420055+maitanchinh@users.noreply.github.com>
2024-02-01 13:49:25 +0700

    fix

--- crosify_main/crosify_order/models/product_type_fulfill.py
+++ crosify_main/crosify_order/models/product_type_fulfill.py
@@ -4,7 +4,7 @@ class ProductTypeFulfill(models.Model):
     _name = 'sale.order.product.type.fulfill'
     _description = 'Product Type - Fulfill'
     
-    product_type = fields.Many2one('product.template', string='Product Type')
-    product_vendor = fields.Many2one('res.partner', string='Product Vendor')
-    packing_vendor = fields.Many2one('res.partner', string='Packing Vendor')
-    shipping_vendor = fields.Many2one('res.partner', string='Shipping Vendor')
+    product_type_id = fields.Many2one('product.template', string='Product Type')
+    product_vendor_id = fields.Many2one('res.partner', string='Product Vendor')
+    packing_vendor_id = fields.Many2one('res.partner', string='Packing Vendor')
+    shipping_vendor_id = fields.Many2one('res.partner', string='Shipping Vendor')
