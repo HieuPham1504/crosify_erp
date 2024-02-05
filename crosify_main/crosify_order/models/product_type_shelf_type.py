5272341f07140f1a78764c6b110bb347cd184285
maitanchinh <64420055+maitanchinh@users.noreply.github.com>
2024-02-05 11:34:57 +0700

    product type - shelf type

--- crosify_main/crosify_order/models/product_type_shelf_type.py
+++ crosify_main/crosify_order/models/product_type_shelf_type.py
@@ -5,14 +5,14 @@ class ProductTypeShelfType(models.Model):
     _name = 'product.type.shelf.type'
     _description = 'Product Type - Shelf Type'
 
-    product_type_id = fields.Many2one('product.template', string='Product Type', required=True)
+    product_tmp_id = fields.Many2one('product.template', string='Product Type', required=True)
     shelf_type_id = fields.Many2one('fulfill.shelf.type', string='Shelf Type', required=True)
-    print(product_type_id)
-    @api.constrains('product_type_id')
+
+    @api.constrains('product_tmp_id')
     def _check_product_type(self):
         for rec in self.sudo():
-                product_type_id = rec.product_type_id.id
+                product_tmp_id = rec.product_tmp_id.id
                 duplicate_type = self.sudo().search(
-                    [('id', '!=', rec.id), ('product_type_id', '=', product_type_id)])
+                    [('id', '!=', rec.id), ('product_tmp_id', '=', product_tmp_id)])
                 if duplicate_type:
                     raise ValidationError(_("This product type has been used for another shelf type.")) 
