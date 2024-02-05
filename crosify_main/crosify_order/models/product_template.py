55acf5b84a500eaffaaa476036d755464c74615a
Phạm Trọng Hiếu <81959458+HieuPham1504@users.noreply.github.com>
2024-02-05 13:36:11 +0700

    sku

--- crosify_main/crosify_order/models/product_template.py
+++ crosify_main/crosify_order/models/product_template.py
@@ -162,4 +162,23 @@ class ProductTemplate(models.Model):
         sku_suffix = self.env['ir.sequence'].sudo().next_by_code('product.product.sku') or '_Undefined'
         variant_dict['default_code'] = f'{self.product_type}{sku_suffix}'
         return variant_dict
+
+    def _set_product_variant_field(self, fname):
+        """Propagate the value of the given field from the templates to their unique variant.
+
+        Only if it's a single variant product.
+        It's used to set fields like barcode, weight, volume..
+
+        :param str fname: name of the field whose value should be propagated to the variant.
+            (field name must be identical between product.product & product.template models)
+        """
+        for template in self:
+            count = len(template.product_variant_ids)
+            if count == 1:
+                # template.product_variant_ids[fname] = template[fname]
+                continue
+            elif count == 0:
+                archived_variants = self.with_context(active_test=False).product_variant_ids
+                if len(archived_variants) == 1:
+                    archived_variants[fname] = template[fname]
         
