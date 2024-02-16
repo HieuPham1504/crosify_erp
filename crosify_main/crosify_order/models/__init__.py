e7a23fd1a25c80958f5cfe9019f3620b6c8887bf
Phạm Trọng Hiếu <81959458+HieuPham1504@users.noreply.github.com>
2024-02-16 17:01:01 +0700

    production

--- crosify_main/crosify_order/models/__init__.py
+++ crosify_main/crosify_order/models/__init__.py
@@ -11,3 +11,5 @@ from . import fulfill_shelf_type
 from . import fulfill_shelf
 from . import product_type_shelf_type
 from . import sale_order_type
+from . import mo_production
+from . import mo_production_line
