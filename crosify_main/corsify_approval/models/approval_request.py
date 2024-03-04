7a447b05034bcb722123bf410cf1d4d02d34ef46
Phạm Trọng Hiếu <81959458+HieuPham1504@users.noreply.github.com>
2024-03-04 17:12:40 +0700

    Update approval_request.py

--- crosify_main/corsify_approval/models/approval_request.py
+++ crosify_main/corsify_approval/models/approval_request.py
@@ -17,7 +17,7 @@ class ApprovalRequest(models.Model):
         time = fields.Date.to_string(user_tz.localize(fields.Datetime.from_string(datetime.now().date()),
                                                            is_dst=None).astimezone(pytz.utc))
 
-        today_format_split = time.strftime('%d/%d/%Y').split('/')
+        today_format_split = time.split('/')
         date = today_format_split[0]
         month = today_format_split[1]
         year = today_format_split[-1][-2:]
