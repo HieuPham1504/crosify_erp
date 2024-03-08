3031f16a571e471ea752e6c7725c509dee968ee4
Phạm Trọng Hiếu <81959458+HieuPham1504@users.noreply.github.com>
2024-03-08 15:33:08 +0700

    udate order

--- crosify_main/crosify_order/wizard/update_order_wizard.py
+++ crosify_main/crosify_order/wizard/update_order_wizard.py
@@ -9,19 +9,21 @@ from odoo import api, fields, models, _
 from odoo.exceptions import UserError, ValidationError
 
 
-class UpdateItemWizard(models.TransientModel):
-    _name = 'update.item.wizard'
+class UpdateOrderWizard(models.TransientModel):
+    _name = 'update.order.wizard'
     _rec_name = 'name'
 
-    name = fields.Char(default='Update Item')
+    name = fields.Char(default='Update Order')
     item_file = fields.Binary(string='Items File')
     item_file_name = fields.Char(string='Items')
-    line_ids = fields.One2many('update.item.line.wizard', 'update_item_wizard_id', string='Update Items')
+    line_ids = fields.One2many('update.order.line.wizard', 'update_item_wizard_id', string='Update Orders')
 
     def action_import_items(self):
         Items = self.env['sale.order.line'].sudo()
         Products = self.env['product.product'].sudo()
         ItemLines = self.env['update.item.line.wizard'].sudo()
+        States = self.env['res.country.state'].sudo()
+        Country = self.env['res.country'].sudo()
         import_file = self.item_file
         try:
             wb = openpyxl.load_workbook(
@@ -33,9 +35,26 @@ class UpdateItemWizard(models.TransientModel):
             ws = wb.active
             for record in ws.iter_rows(min_row=2, max_row=None, min_col=None,
                                        max_col=None, values_only=True):
-                production_id = record[1]
-                sku = record[2]
-                personalize = record[3]
+                order_id_fix = record[1]
+                first_name = record[2]
+                last_name = record[3]
+                street = record[4]
+                street2 = record[5]
+                city = record[6]
+                state_code = record[7]
+                if not state_code:
+                    state_id = False
+                else:
+                    state_id = States.search([('code', '=', state_code)], limit=1)
+                country_code = record[8]
+                if not country_code:
+                    country_id = False
+                else:
+                    state_id = States.search([('code', '=', state_code)], limit=1)
+                zip = record[9]
+                phone = record[10]
+                email = record[11]
+
                 if not sku or not production_id:
                     continue
                 else:
@@ -69,11 +88,19 @@ class UpdateItemWizard(models.TransientModel):
 
 
 
-class UpdateItemLineWizard(models.TransientModel):
-    _name = 'update.item.line.wizard'
+class UpdateOrderLineWizard(models.TransientModel):
+    _name = 'update.order.line.wizard'
 
     update_item_wizard_id = fields.Many2one('update.item.wizard')
-    production_id = fields.Char(string='Production ID', required=True)
-    sale_order_line_id = fields.Many2one('sale.order.line', string='Item')
-    product_id = fields.Many2one('product.product', string='SKU')
-    personalize = fields.Char(string='Personalize')
+    order_id_fix = fields.Char(string='Order ID Fix', required=True)
+    order_id = fields.Many2one('sale.order', string='Order')
+    first_name = fields.Char(string='First Name')
+    last_name = fields.Char(string='Last Name')
+    street = fields.Char(string='Street 1')
+    street2 = fields.Char(string='Street 2')
+    city = fields.Char(string='City')
+    state_id = fields.Many2one('res.country.state', string='State')
+    zip = fields.Char(string='Zip Code')
+    country_id = fields.Many2one('res.country', string='Country')
+    phone = fields.Char(string='Phone')
+    email = fields.Char(string='Email')
