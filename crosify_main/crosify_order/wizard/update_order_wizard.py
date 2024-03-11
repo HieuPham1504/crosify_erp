7d1208b5b7dbb3ea7a8080478a7fb909c2628f7f
Phạm Trọng Hiếu <81959458+HieuPham1504@users.noreply.github.com>
2024-03-11 10:50:35 +0700

    menu action

--- crosify_main/crosify_order/wizard/update_order_wizard.py
+++ crosify_main/crosify_order/wizard/update_order_wizard.py
@@ -1,4 +1,3 @@
-
 # -*- coding: utf-8 -*-
 import pytz
 import openpyxl
@@ -16,12 +15,12 @@ class UpdateOrderWizard(models.TransientModel):
     name = fields.Char(default='Update Order')
     item_file = fields.Binary(string='Items File')
     item_file_name = fields.Char(string='Items')
-    line_ids = fields.One2many('update.order.line.wizard', 'update_item_wizard_id', string='Update Orders')
+    line_ids = fields.One2many('update.order.line.wizard', 'update_order_wizard_id', string='Update Orders')
 
     def action_import_items(self):
         Items = self.env['sale.order.line'].sudo()
         Products = self.env['product.product'].sudo()
-        ItemLines = self.env['update.item.line.wizard'].sudo()
+        ItemLines = self.env['update.order.line.wizard'].sudo()
         States = self.env['res.country.state'].sudo()
         Country = self.env['res.country'].sudo()
         import_file = self.item_file
@@ -31,7 +30,7 @@ class UpdateOrderWizard(models.TransientModel):
                 filename=BytesIO(base64.b64decode(import_file)), read_only=True
             )
             item_file_datas = []
-            production_ids = []
+            order_id_fixes = []
             ws = wb.active
             for record in ws.iter_rows(min_row=2, max_row=None, min_col=None,
                                        max_col=None, values_only=True):
@@ -42,34 +41,40 @@ class UpdateOrderWizard(models.TransientModel):
                 street2 = record[5]
                 city = record[6]
                 state_code = record[7]
-                if not state_code:
-                    state_id = False
-                else:
-                    state_id = States.search([('code', '=', state_code)], limit=1)
                 country_code = record[8]
                 if not country_code:
                     country_id = False
                 else:
-                    state_id = Country.search([('code', '=', country_code)], limit=1)
+                    state_id = Country.search([('code', '=', country_code)], limit=1).id
+                if not state_code:
+                    state_id = False
+                else:
+                    state_id = States.search([('code', '=', state_code), ('country_id', '=', country_id)], limit=1).id
                 zip = record[9]
                 phone = record[10]
                 email = record[11]
 
-                if not sku or not production_id:
+                if not order_id_fix:
                     continue
                 else:
-                    if production_id and production_id not in production_ids:
-                        production_id = production_id.strip()
-                        production_ids.append(production_id)
-                        item = Items.search([('production_id', '=', production_id)], limit=1)
-                        product_id = Products.search([('default_code', '=', sku.strip())], limit=1)
-                        if item and product_id:
-                            item_file_datas.append({
-                                'production_id': production_id,
-                                'sale_order_line_id': item.id,
-                                'product_id': product_id.id,
-                                'personalize': personalize,
-                            })
+                    order_id_fix = order_id_fix.strip()
+                    order_id_fixes.append(order_id_fix)
+                    order = Items.search([('order_id_fix', '=', order_id_fix)], limit=1)
+                    if order:
+                        item_file_datas.append({
+                            'order_id_fix': order_id_fix,
+                            'order_id': order.id,
+                            'first_name': first_name,
+                            'last_name': last_name,
+                            'street': street,
+                            'street2': street2,
+                            'city': city,
+                            'state_id': state_id,
+                            'country_id': country_id,
+                            'zip': zip,
+                            'phone': phone,
+                            'email': email,
+                        })
 
             item_ids = ItemLines.create(item_file_datas)
             self.line_ids = [(6, 0, item_ids.ids)]
@@ -80,18 +85,26 @@ class UpdateOrderWizard(models.TransientModel):
     def action_update_item(self):
         line_ids = self.line_ids
         for line in line_ids:
-            item = line.sale_order_line_id
-            item.write({
-                'product_id': line.product_id.id,
-                'personalize': line.personalize
-            })
-
+            order_id = line.order_id
+            if order_id and order_id.partner_id:
+                order_id.partner_id.write({
+                    'first_name': line.first_name,
+                    'last_name': line.last_name,
+                    'street': line.street,
+                    'street2': line.street2,
+                    'city': line.city,
+                    'state_id': line.state_id.id,
+                    'country_id': line.country_id.id,
+                    'zip': line.zip,
+                    'phone': line.phone,
+                    'email': line.email,
+                })
 
 
 class UpdateOrderLineWizard(models.TransientModel):
     _name = 'update.order.line.wizard'
 
-    update_item_wizard_id = fields.Many2one('update.item.wizard')
+    update_order_wizard_id = fields.Many2one('update.order.wizard')
     order_id_fix = fields.Char(string='Order ID Fix', required=True)
     order_id = fields.Many2one('sale.order', string='Order')
     first_name = fields.Char(string='First Name')
