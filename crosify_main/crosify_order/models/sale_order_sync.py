351af8ade63afe0e8e8396604ede6bb3eb74493f
Phạm Trọng Hiếu <81959458+HieuPham1504@users.noreply.github.com>
2024-03-15 15:42:25 +0700

    update api

--- crosify_main/crosify_order/models/sale_order_sync.py
+++ crosify_main/crosify_order/models/sale_order_sync.py
@@ -3,11 +3,17 @@ from odoo import api, fields, models
 class SaleOrderSync(models.Model):
     _name = 'sale.order.sync'
     _description = 'Sale Order Sync'
+    _order = 'create_date desc'
 
     sale_order_id = fields.Many2one('sale.order', string='Order')
     description = fields.Text(string='Description')
+    description_json = fields.Json(string='Description JSON')
     status = fields.Selection([
         ('pass', 'Pass'),
         ('fail', 'Fail')], string='Status')
     response = fields.Text(string='Response')
+
+    type = fields.Selection([
+        ('create', 'Create'),
+        ('update', 'Update')], string='Type')
 
